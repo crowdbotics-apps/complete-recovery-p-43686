14d6efd07ae3995f2f7b273228c4ce41b02f4691
Crowdbotics <team@crowdbotics.com>
2023-10-01 03:23:01 +0000

    Initial commit

---
+++ backend/complete_recovery_p_43686/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for complete_recovery_p_43686 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "complete_recovery_p_43686.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "complete-recovery-p-43686",
+  url: "https://complete-recovery-p-43686.botics.co",
+  api: "https://complete-recovery-p-43686.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
